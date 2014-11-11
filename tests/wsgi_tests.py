8cf62fea31acc484c2248700f509e24ad2ab7ae3
Rafael Pivato <rafael@pivato.info>
2014-11-11 19:59:57 -0200

    WSGI test file

---
+++ tests/wsgi_tests.py
@@ -0,0 +1,27 @@
+import mock
+import unittest
+
+from resto.wsgi import Middleware
+
+
+class AppTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.environ = {
+            'wsgi.version': (1, 0),
+            'wsgi.multithread': False,
+            'wsgi.multiprocess': False,
+            'wsgi.run_once': True,
+        }
+        self.mock_start = mock.Mock()
+
+    def test_application_init(self):
+        result = Middleware(self.environ, self.mock_start)
+        content = ''
+        for data in result:
+            content += data
+        self.assertGreater(content, '')
+
+
+if __name__ == '__main__':
+    unittest.main()

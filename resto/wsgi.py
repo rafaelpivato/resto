f284ff1145900c969995d4e466b7b395b65980bd
Rafael Pivato <rafael@pivato.info>
2014-11-11 20:55:44 -0200

    WSGI ref server used

--- resto/wsgi.py
+++ resto/wsgi.py
@@ -5,6 +5,8 @@ resto object and resources.
 
 """
 
+from wsgiref.simple_server import make_server
+
 
 class Middleware(object):
     """WSGI middleware that will route to resources."""
@@ -18,3 +20,9 @@ class Middleware(object):
         response_headers = [('Content-type', 'application/json')]
         self.start(status, response_headers)
         yield '{"id": 12345}'
+
+
+if __name__ == '__main__':
+    server = make_server('', 5678, Middleware)
+    print "Starting WSGI non-production server on port 5678..."
+    server.serve_forever()
